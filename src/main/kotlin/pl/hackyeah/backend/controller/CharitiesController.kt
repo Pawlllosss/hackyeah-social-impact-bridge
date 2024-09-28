0371aecb8bf784df1a76cfc78efc9240dbbcaa85
Pawlllosss <oczadlypawe@gmail.com>
2024-09-29 01:18:27 +0200

    get endpoints

--- src/main/kotlin/pl/hackyeah/backend/controller/CharitiesController.kt
+++ src/main/kotlin/pl/hackyeah/backend/controller/CharitiesController.kt
@@ -1,6 +1,8 @@
 package pl.hackyeah.backend.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,5 +19,12 @@ class CharitiesController(private val charitiesService: CharitiesService) {
         val corporateId = charitiesService.createCharity(charity)
 
         return corporateId?.let { ResponseEntity.ok(CharitiesResponseDto(it)) } ?: ResponseEntity.notFound().build()
+    }
+
+    @GetMapping("/{id}")
+    fun getCorporation(@PathVariable id: String): ResponseEntity<Charity> {
+        val charity = charitiesService.getCharity(id)
+
+        return charity?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
     }
 }
--- src/main/kotlin/pl/hackyeah/backend/controller/CorporationsController.kt
+++ src/main/kotlin/pl/hackyeah/backend/controller/CorporationsController.kt
@@ -1,6 +1,8 @@
 package pl.hackyeah.backend.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,5 +19,12 @@ class CorporationsController(private val corporationService: CorporationService)
         val corporateId = corporationService.createCorporation(corporation)
 
         return corporateId?.let { ResponseEntity.ok(CorporationResponseDto(it)) } ?: ResponseEntity.notFound().build()
+    }
+
+    @GetMapping("/{id}")
+    fun getCorporation(@PathVariable id: String): ResponseEntity<Corporation> {
+        val corporation = corporationService.getCorporation(id)
+
+        return corporation?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
     }
 }
